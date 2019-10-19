841fcb900ebbb657abdb6433306579ddb7fca175
Lee <873793992@qq.com>
2019-10-19 15:54:35 +0800

    blog列表

--- src/container/Java/index.js
+++ src/container/Java/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import CustomBreadcrumb from '../../component/CustomBreadcrumb/index'
-import TypingCard from '../../component/TypingCard'
+import BlogList from "../../component/BlogList/Index";
 
-export default class Java extends React.Component{
-  render(){
-    return (
-      <div>
-        <CustomBreadcrumb arr={['Java']}/>
-        <TypingCard source={'Java文档'} title='Java' />
-      </div>
-    )
-  }
+export default class Java extends React.Component {
+    render() {
+        return (
+            <div>
+                <CustomBreadcrumb arr={['Java']}/>
+                <BlogList/>
+            </div>
+        )
+    }
 }
